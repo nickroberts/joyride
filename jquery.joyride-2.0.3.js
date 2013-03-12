9d85c2a4bff4b22ee52473580ae7758a72fb45ce
Nick Schuch <nick@previousnext.com.au>
2013-03-11 22:41:10 -0700

    Adds support for a 'Previous' button.
    Conflicts:
    	jquery.joyride-2.0.3.js

--- jquery.joyride-2.0.3.js
+++ jquery.joyride-2.0.3.js
@@ -21,6 +21,7 @@
       'autoStart'            : false,     // true or false - false tour starts when restart called
       'startTimerOnClick'    : true,      // true or false - true requires clicking the first button start the timer
       'startOffset'          : 0,         // the index of the tooltip you want to start on (index of the li)
+      'prevButton'           : true,      // true or false to control whether a prev button is used
       'nextButton'           : true,      // true or false to control whether a next button is used
       'tipAnimation'         : 'fade',    // 'pop' or 'fade' in each tip
       'pauseAfter'           : [],        // array of indexes where to pause the tour after
@@ -41,10 +42,11 @@
         'timer'   : '<div class="joyride-timer-indicator-wrap"><span class="joyride-timer-indicator"></span></div>',
         'tip'     : '<div class="joyride-tip-guide"><span class="joyride-nub"></span></div>',
         'wrapper' : '<div class="joyride-content-wrapper" role="dialog"></div>',
-        'button'  : '<a href="#" class="joyride-next-tip"></a>',
         'modal'   : '<div class="joyride-modal-bg"></div>',
         'expose'  : '<div class="joyride-expose-wrapper"></div>',
         'exposeCover': '<div class="joyride-expose-cover"></div>'
+        'prevButton'  : '<a href="#" class="joyride-prev-tip"></a>',
+        'nextButton'  : '<a href="#" class="joyride-next-tip"></a>'
       }
     },
 
@@ -123,6 +125,23 @@
 
             });
 
+            settings.$document.on('click.joyride', '.joyride-prev-tip', function (e) {
+              e.preventDefault();
+
+              if (settings.$li.prev().length < 1) {
+                methods.end();
+              } else if (settings.timer > 0) {
+                clearTimeout(settings.automate);
+                methods.hide();
+                methods.show();
+                methods.startTimer();
+              } else {
+                methods.hide();
+                methods.show(false, true);
+              }
+
+            });
+
             settings.$document.on('click.joyride', '.joyride-close-tip', function (e) {
               e.preventDefault();
               methods.end();
@@ -177,7 +196,8 @@
 
         $blank = $(settings.template.tip).addClass(opts.tip_class);
         content = $.trim($(opts.li).html()) +
-          methods.button_text(opts.button_text) +
+          methods.prev_button_text(opts.next_button_text, opts.index) +
+          methods.next_button_text(opts.prev_button_text) +
           settings.template.link +
           methods.timer_instance(opts.index);
 
@@ -206,10 +226,20 @@
         return txt;
       },
 
-      button_text : function (txt) {
+      next_button_text : function (txt) {
         if (settings.nextButton) {
           txt = $.trim(txt) || 'Next';
-          txt = methods.outerHTML($(settings.template.button).append(txt)[0]);
+          txt = methods.outerHTML($(settings.template.nextButton).append(txt)[0]);
+        } else {
+          txt = '';
+        }
+        return txt;
+      },
+
+      prev_button_text : function (txt, index) {
+        if (settings.prevButton && index > 0) {
+          txt = $.trim(txt) || 'Previous';
+          txt = methods.outerHTML($(settings.template.prevButton).append(txt)[0]);
         } else {
           txt = '';
         }
@@ -218,19 +248,21 @@
 
       create : function (opts) {
         // backwards compatibility with data-text attribute
-        var buttonText = opts.$li.attr('data-button') || opts.$li.attr('data-text'),
-          tipClass = opts.$li.attr('class'),
-          $tip_content = $(methods.tip_template({
-            tip_class : tipClass,
-            index : opts.index,
-            button_text : buttonText,
-            li : opts.$li
-          }));
+        var nextButtonText = opts.$li.attr('data-next-button') || opts.$li.attr('data-button') || opts.$li.attr('data-text');
+        var prevButtonText = opts.$li.attr('data-prev-button');
+        var tipClass = opts.$li.attr('class');
+        var $tip_content = $(methods.tip_template({
+          tip_class : tipClass,
+          index : opts.index,
+          next_button_text : nextButtonText,
+          prev_button_text : prevButtonText,
+          li : opts.$li
+        }));
 
         $(settings.tipContainer).append($tip_content);
       },
 
-      show : function (init) {
+      show : function (init, prev) {
         var opts = {}, ii, opts_arr = [], opts_len = 0, p,
             $timer = null;
 
@@ -241,7 +273,7 @@
           if (settings.paused) {
             settings.paused = false;
           } else {
-            methods.set_li(init);
+            methods.set_li(init, prev);
           }
 
           settings.attempts = 0;
@@ -367,13 +399,18 @@
         settings.postStepCallback(settings.$li.index(), settings.$current_tip);
       },
 
-      set_li : function (init) {
+      set_li : function (init, prev) {
         if (init) {
           settings.$li = settings.$tip_content.eq(settings.startOffset);
           methods.set_next_tip();
           settings.$current_tip = settings.$next_tip;
         } else {
-          settings.$li = settings.$li.next();
+          if (prev) {
+            settings.$li = settings.$li.prev();
+          }
+          else {
+            settings.$li = settings.$li.next();
+          }
           methods.set_next_tip();
         }
 
